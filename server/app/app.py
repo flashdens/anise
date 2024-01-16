75da24612024045b7613a0aa382496b8dd2aa4ed
flashdens <metis234@gmail.com>
2024-01-16 11:38:39 +0100

    added show/hide rack and move buttons

--- server/app/app.py
+++ server/app/app.py
@@ -42,12 +42,12 @@ def message():
 @app.route('/api/draw')
 def test():
     tiles = [
-        {"symbol": "A", "color": "yellow"},
-        {"symbol": "B", "color": "blue"},
-        {"symbol": "C", "color": "red"},
-        {"symbol": "D", "color": "green"},
-        {"symbol": "E", "color": "orange"},
-        {"symbol": "F", "color": "purple"}
+        {"symbol": "A", "color": "yellow", "id": 1},
+        {"symbol": "B", "color": "blue", "id": 2},
+        {"symbol": "C", "color": "red", "id": 3},
+        {"symbol": "D", "color": "green", "id": 4},
+        {"symbol": "E", "color": "orange", "id": 5},
+        {"symbol": "F", "color": "purple", "id": 6}
     ]
     return jsonify(tiles)
 
